bd3157fa8e944a6dd86921355d139fa73b37c13e
Emmanuel Ramos <emmanuelframos@gmail.com>
2018-10-13 04:43:10 -0300

    Changing ip

--- static/app/services/game-service.js
+++ static/app/services/game-service.js
@@ -2,22 +2,22 @@ angular.module('e-sports').service('GameService', function($http){
     var self = this;
 
     self.getGames = function(){
-        return $http.get('http://localhost:5000/game/all');
+        return $http.get('http://ec2-18-216-2-201.us-east-2.compute.amazonaws.com:5000/game/all');
     }
 
     self.getGame = function(game_id){
-        return $http.get('http://localhost:5000/game/' + game_id);
+        return $http.get('http://ec2-18-216-2-201.us-east-2.compute.amazonaws.com:5000/game/' + game_id);
     }
 
     self.saveGame = function(game){
-        return $http.post('http://localhost:5000/game/create', game);
+        return $http.post('http://ec2-18-216-2-201.us-east-2.compute.amazonaws.com:5000/game/create', game);
     }
 
     self.updateGame = function(game){
-        return $http.put('http://localhost:5000/game/update', game);
+        return $http.put('http://ec2-18-216-2-201.us-east-2.compute.amazonaws.com:5000/game/update', game);
     }
 
     self.deleteGame = function(game_id){
-        return $http.delete('http://localhost:5000/game/remove/' + game_id);
+        return $http.delete('http://ec2-18-216-2-201.us-east-2.compute.amazonaws.com:5000/game/remove/' + game_id);
     }
 });
